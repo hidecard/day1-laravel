c94b73b55dab2512f0af12d6d67918527da4cda4
hidecard <hidecard1500@gmail.com>
2025-05-15 11:07:18 +0630

    day1

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::resource('/post',PostController::class);
