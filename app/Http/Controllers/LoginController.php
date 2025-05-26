3d9475588fc257948ed69da8c0a0bf6b4f62a7c0
hidecard <hidecard1500@gmail.com>
2025-05-26 09:53:31 +0630

    day 9

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    //
+    public function showLogin(){
+        return view('frontend.login');
+    }
+    public function login(Request  $request){
+        $credentials = $request -> validate([
+            'email' => ['required','email'],
+            'password' => ['required']
+        ]);
+        if(Auth::attempt($credentials)){
+            $request->session()->regenerate();  
+            return redirect()->intended('post/list') ;
+        }
+    }
 }
